e76c9794231e41f72c5688fd91df1a02925f10e3
Madsthunder <madsthunder109@gmail.com>
2016-12-26 21:43:10 -0500

    Renamed some fields to fit the new Characteristics.

--- src/main/java/api/metalextras/OreType.java
+++ src/main/java/api/metalextras/OreType.java
@@ -30,114 +30,116 @@ public abstract class OreType implements Comparable<OreType>
 	private ResourceLocation name;
 	private final OreTypes types;
 	private final IBlockState state;
-	private final List<OreTypeDictionary> entries;
+	private final List<Characteristic> characteristics;
 	private int id = -1;
-	
-	public OreType(OreTypes types, IBlockState state, OreTypeDictionary... entries)
+
+	public OreType(OreTypes types, IBlockState state, Characteristic... characteristics)
 	{
 		this.state = state;
 		this.types = types;
-		this.entries = Lists.newArrayList(entries);
+		this.characteristics = Lists.newArrayList(characteristics);
 	}
-	
+
 	public IBlockState getState()
 	{
 		return this.state;
 	}
-	
+
 	public abstract float getHardness();
-	
+
 	public abstract float getResistance();
-	
+
 	public abstract String getLanguageKey();
-	
+
 	public OreTypes getTypes()
 	{
 		return this.types;
 	}
-	
+
 	public SoundType getSoundType()
 	{
 		return this.getState().getBlock().getSoundType();
 	}
-	
+
 	public String getHarvestTool()
 	{
 		return this.getState().getBlock().getHarvestTool(this.getState());
 	}
-	
+
 	public Material getMaterial()
 	{
 		return this.getState().getBlock().getMaterial(this.getState());
 	}
-	
+
 	public int getHarvestLevel()
 	{
 		return this.getState().getBlock().getHarvestLevel(this.getState());
 	}
-	
+
 	public boolean canFall()
 	{
 		return this.getState().getBlock() instanceof BlockFalling;
 	}
-	
+
+	@Override
 	public int compareTo(OreType o)
 	{
 		List<OreType> list = OreUtils.getAllOreTypes();
 		return list.indexOf(this) - list.indexOf(o);
 	}
-	
+
 	public void handleEntityCollision(World world, BlockPos pos, IBlockState state, Entity entity)
 	{
-		
+
 	}
-	
+
 	public AxisAlignedBB getSelectionBox(World world, BlockPos pos)
 	{
 		return Block.FULL_BLOCK_AABB;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	public abstract IModel getModel(OreMaterial material);
-	
+
 	public final OreType setRegistryName(String modid, String name)
 	{
 		return this.setRegistryName(new ResourceLocation(modid, name));
 	}
-	
+
 	public final OreType setRegistryName(String name)
 	{
 		return this.setRegistryName(new ResourceLocation(name));
 	}
-	
+
 	public final OreType setRegistryName(ResourceLocation name)
 	{
-		if(this.getRegistryName() != null)
-			throw new IllegalStateException("Attempted to set registry name with existing registry name! New: " + name + " Old: " + this.getRegistryName());
+		if (this.getRegistryName() != null)
+			throw new IllegalStateException("Attempted to set registry name with existing registry name! New: " + name
+					+ " Old: " + this.getRegistryName());
 		this.name = name;
 		return this;
 	}
-	
+
 	public final ResourceLocation getRegistryName()
 	{
 		return this.name;
 	}
-	
+
 	public final Class<? super OreType> getRegistryType()
 	{
 		return OreType.class;
 	}
-	
-	public Collection<OreTypeDictionary> getOreTypeDictionaryList()
+
+	public Collection<Characteristic> getCharacteristics()
 	{
-		return Sets.newHashSet(this.entries);
+		return Sets.newHashSet(this.characteristics);
 	}
-	
+
 	public final void update()
 	{
 		this.id = OreUtils.getID(this);
 	}
-	
+
 	public static class Impl extends OreType
 	{
 		private float hardness = 0;
@@ -145,70 +147,73 @@ public abstract class OreType implements Comparable<OreType>
 		private String langKey;
 		@SideOnly(Side.CLIENT)
 		private ResourceLocation texture;
-		
-		public Impl(OreTypes types, IBlockState state, OreTypeDictionary... entries)
+
+		public Impl(OreTypes types, IBlockState state, Characteristic... characteristics)
 		{
-			super(types, state, entries);
+			super(types, state, characteristics);
 			this.langKey = state.getBlock().getUnlocalizedName() + ".name";
 		}
-		
+
 		public OreType.Impl setHardness(float hardness)
 		{
 			this.hardness = hardness;
 			return this;
 		}
-		
+
 		@Override
 		public float getHardness()
 		{
 			return this.hardness;
 		}
-		
+
 		public OreType.Impl setResistance(float resistance)
 		{
 			this.resistance = resistance * 3;
 			return this;
 		}
-		
+
 		@Override
 		public float getResistance()
 		{
 			return this.resistance * 5;
 		}
-		
+
 		public OreType.Impl setLanguageKey(String langKey)
 		{
 			this.langKey = langKey;
 			return this;
 		}
-		
+
+		@Override
 		public String getLanguageKey()
 		{
 			return this.langKey;
 		}
-		
+
 		public OreType.Impl setModelTexture(ResourceLocation texture)
 		{
-			if(FMLLaunchHandler.side() == Side.CLIENT)
+			if (FMLLaunchHandler.side() == Side.CLIENT)
 				this.texture = texture;
 			return this;
 		}
-		
+
+		@Override
 		@SideOnly(Side.CLIENT)
 		public IModel getModel(OreMaterial material)
 		{
 			return getModelFromTexture(this.texture);
 		}
-		
+
 		public static IModel getModelFromTexture(ResourceLocation texture)
 		{
 			IModel missing = ModelLoaderRegistry.getMissingModel();
 			IModel model = ModelLoaderRegistry.getModelOrMissing(new ResourceLocation("minecraft:block/cube_all"));
-			if(model != missing && model instanceof IRetexturableModel)
+			if (model != missing && model instanceof IRetexturableModel)
 			{
-				if(model instanceof IModelUVLock)
-					model = ((IModelUVLock)model).uvlock(true);
-				return ((IRetexturableModel)model).retexture(new ImmutableMap.Builder().put("all", texture.toString()).build());
+				if (model instanceof IModelUVLock)
+					model = ((IModelUVLock) model).uvlock(true);
+				return ((IRetexturableModel) model)
+						.retexture(new ImmutableMap.Builder().put("all", texture.toString()).build());
 			}
 			return missing;
 		}
