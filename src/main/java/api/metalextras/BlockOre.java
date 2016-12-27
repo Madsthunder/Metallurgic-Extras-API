28cbde2f9b655cef3e69d77ef287d459a2ca5b1a
Madsthunder <madsthunder109@gmail.com>
2016-12-26 21:43:34 -0500

    Added support for landing particles.

--- src/main/java/api/metalextras/BlockOre.java
+++ src/main/java/api/metalextras/BlockOre.java
@@ -5,8 +5,12 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
+import api.metalextras.SPacketBlockOreLandingParticles.SendLandingParticlesEvent;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFalling;
 import net.minecraft.block.SoundType;
@@ -30,30 +34,44 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockOre extends BlockFalling
+public abstract class BlockOre extends BlockFalling
 {
 	private final OreTypeProperty property;
 	private final OreMaterial material;
 	
 	public BlockOre(OreMaterial material, OreTypes types)
 	{
+		this(material, types, getDefaultRegistryNameGetter());
+	}
+	
+	public BlockOre(OreMaterial material, OreTypes types, Function<Pair<OreMaterial, OreTypes>, String> registry_name_getter)
+	{
 		super(Material.ROCK);
-		this.setRegistryName(material.getRegistryName().toString() + "." + types.getRegistryName().toString().replaceFirst(":", "_"));
+		String s = registry_name_getter.apply(Pair.of(material, types));
+		if(!s.startsWith(material.getRegistryName().getResourceDomain()))
+			throw new IllegalStateException("String \"" + s + "\" (From Function \"" + registry_name_getter + "\") Doesn't Begin With CharSequence \"" + material.getRegistryName().getResourceDomain() + "\".");
+		if(!s.contains(material.getRegistryName().getResourcePath()))
+			throw new IllegalStateException("String \"" + s + "\" (From Function \"" + registry_name_getter + "\") Doesn't Contain CharSequence \"" + material.getRegistryName().getResourcePath() + "\".");
+		if(!s.contains(types.getRegistryName().toString().replaceFirst(":", "_")))
+			throw new IllegalStateException("String \"" + s + "\" (From Function \"" + registry_name_getter + "\") Doesn't Contain CharSequence \"" + types.getRegistryName().toString().replaceFirst(":", "_") + "\".");
+		this.setRegistryName(s);
 		this.property = new OreTypeProperty(types, this);
 		this.setDefaultState(this.getBlockState().getBaseState());
 		this.setDefaultState(this.getBlockState().getProperties().isEmpty() ? this.getDefaultState() : this.getDefaultState().withProperty(this.getOreTypeProperty(), this.getOreTypeProperty().getAllowedValues().get(0)));
 		this.material = material;
 	}
 	
-	public OreMaterial getOreMaterial()
+	public final OreMaterial getOreMaterial()
 	{
 		return this.material;
 	}
@@ -65,7 +83,7 @@ public class BlockOre extends BlockFalling
 	}
 	
 	@Override
-	public final SoundType getSoundType(IBlockState state, World world, BlockPos pos, Entity entity)
+	public SoundType getSoundType(IBlockState state, World world, BlockPos pos, Entity entity)
 	{
 		return this.getOreType(state).getSoundType();
 	}
@@ -104,46 +122,23 @@ public class BlockOre extends BlockFalling
 	}
 	
 	@Override
-	public ItemStack getPickBlock(IBlockState state, RayTraceResult result, World world, BlockPos pos, EntityPlayer player)
-	{
-		return new ItemStack(this, 1, this.getMetaFromState(state));
-	}
-	
-	@Override
-	public Item getItemDropped(IBlockState state, Random random, int fortune)
-	{
-		return this.material.getDrop();
-	}
-	
-	@Override
-	public int quantityDropped(Random random)
-	{
-		return this.material.getDropCount(random);
-	}
-	
-	@Override
-	public int quantityDroppedWithBonus(int fortune, Random random)
-	{
-		return this.material.getDropCountWithFortune(fortune, random);
-	}
-	
-	@Override
-	public int damageDropped(IBlockState state)
-	{
-		int meta = this.material.getDropMeta();
-		return meta == -1 ? OreUtils.getItemStackForMaterial(this.material).getMetadata() : meta;
-	}
-	
-	@Override
-	public int getExpDrop(IBlockState state, IBlockAccess access, BlockPos pos, int fortune)
-	{
-		return this.material.getDropXP(access instanceof World ? ((World)access).rand : Block.RANDOM);
-	}
-	
-	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced)
 	{
 		tooltip.add((this.getOreType(stack.getMetadata()).getState().getBlock().getLocalizedName()));
+	}
+	
+	@Override
+	public int getHarvestLevel(IBlockState state)
+	{
+		int materialHarvest = this.getOreMaterial().getHarvestLevel();
+		int typeHarvest = this.getOreType(state).getHarvestLevel();
+		return materialHarvest == -1 || typeHarvest == -1 ? -1 : Math.max(materialHarvest, typeHarvest);
+	}
+	
+	@Override
+	public String getHarvestTool(IBlockState state)
+	{
+		return this.getOreType(state).getHarvestTool();
 	}
 	
 	@Override
@@ -165,50 +160,42 @@ public class BlockOre extends BlockFalling
 	}
 	
 	@Override
-	public int getHarvestLevel(IBlockState state)
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult result, World world, BlockPos pos, EntityPlayer player)
 	{
-		int materialHarvest = this.getOreMaterial().getHarvestLevel();
-		int typeHarvest = this.getOreType(state).getHarvestLevel();
-		return materialHarvest == -1 || typeHarvest == -1 ? -1 : Math.max(materialHarvest, typeHarvest);
-	}
-	
-	@Override
-	public String getHarvestTool(IBlockState state)
-	{
-		return this.getOreType(state).getHarvestTool();
+		return new ItemStack(this, 1, this.getMetaFromState(state));
 	}
 	
 	@Override
 	public void onBlockAdded(World world, BlockPos pos, IBlockState state)
 	{
-		if(state.getValue(this.property).canFall())
+		if(this.getOreType(state).canFall())
 			world.scheduleUpdate(pos, this, this.tickRate(world));
 	}
 	
 	@Override
 	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block block, BlockPos neighbor)
 	{
-		if(state.getValue(this.property).canFall())
+		if(this.getOreType(state).canFall())
 			world.scheduleUpdate(pos, this, this.tickRate(world));
 	}
 	
 	@Override
-	public void getSubBlocks(Item item, CreativeTabs tab, NonNullList<ItemStack> list)
+	public final void getSubBlocks(Item item, CreativeTabs tab, NonNullList<ItemStack> list)
 	{
-		for(int i = 0; i < this.property.getAllowedValues().size(); i++)
+		for(int i = 0; i < this.getOreTypeProperty().getTypes().getOreTypes().size(); i++)
 			list.add(new ItemStack(item, 1, i));
 	}
 	
 	@Override
 	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity)
 	{
-		state.getValue(this.property).handleEntityCollision(world, pos, state, entity);
+		this.getOreType(state).handleEntityCollision(world, pos, state, entity);
 	}
 	
 	@Override
 	public void updateTick(World world, BlockPos pos, IBlockState state, Random random)
 	{
-		if(state.getValue(this.property).canFall())
+		if(this.getOreType(state).canFall())
 			super.updateTick(world, pos, state, random);
 	}
 	
@@ -269,9 +256,9 @@ public class BlockOre extends BlockFalling
 		int k = pos.getZ();
 		float f = 0.1F;
 		AxisAlignedBB axisalignedbb = state.getBoundingBox(world, pos);
-		double d0 = (double)i + world.rand.nextDouble() * (axisalignedbb.maxX - axisalignedbb.minX - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minX;
-		double d1 = (double)j + world.rand.nextDouble() * (axisalignedbb.maxY - axisalignedbb.minY - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minY;
-		double d2 = (double)k + world.rand.nextDouble() * (axisalignedbb.maxZ - axisalignedbb.minZ - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minZ;
+		double d0 = i + world.rand.nextDouble() * (axisalignedbb.maxX - axisalignedbb.minX - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minX;
+		double d1 = j + world.rand.nextDouble() * (axisalignedbb.maxY - axisalignedbb.minY - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minY;
+		double d2 = k + world.rand.nextDouble() * (axisalignedbb.maxZ - axisalignedbb.minZ - 0.20000000298023224D) + 0.10000000149011612D + axisalignedbb.minZ;
 		
 		switch(side)
 		{
@@ -319,15 +306,17 @@ public class BlockOre extends BlockFalling
 		return true;
 	}
 	
+	@Override
 	public boolean addLandingEffects(IBlockState state, WorldServer world, BlockPos pos, IBlockState useless, EntityLivingBase entity, int particles)
 	{
+		MinecraftForge.EVENT_BUS.post(new SendLandingParticlesEvent(world.provider.getDimension(), new SPacketBlockOreLandingParticles(this.getOreMaterial(), this.getOreType(state), new Vec3d(entity.posX, entity.posY, entity.posZ), particles)));
 		return true;
 	}
 	
 	@Override
 	public CreativeTabs getCreativeTabToDisplayOn()
 	{
-		return this.material.getCreativeTab();
+		return this.getOreMaterial().getCreativeTab();
 	}
 	
 	public OreTypeProperty getOreTypeProperty()
@@ -354,42 +343,98 @@ public class BlockOre extends BlockFalling
 	@Override
 	public String getUnlocalizedName()
 	{
-		String langKey = this.material.getLanguageKey();
-		return langKey == null ? this.material.getRegistryName().getResourcePath() : this.material.getLanguageKey();
+		String langKey = this.getOreMaterial().getLanguageKey();
+		return langKey == null ? this.getOreMaterial().getRegistryName().getResourcePath() : this.getOreMaterial().getLanguageKey();
 	}
 	
+	@Override
 	public final int getMetaFromState(IBlockState state)
 	{
 		return this.getIndex(this.getOreType(state));
 	}
 	
+	@Override
 	public final IBlockState getStateFromMeta(int meta)
 	{
 		return this.getBlockState(this.getOreType(meta));
 	}
 	
+	@Override
 	public final BlockStateContainer getBlockState()
 	{
-		return this.property.getBlockState();
+		return this.getOreTypeProperty().getBlockState();
 	}
 	
 	public final OreType getOreType(IBlockState state)
 	{
-		return this.getBlockState().getProperties().isEmpty() ? this.property.getAllowedValues().get(0) : state.getValue(this.property);
+		return this.getBlockState().getProperties().isEmpty() ? this.getOreTypeProperty().getTypes().getOreTypes().get(0) : state.getValue(this.getOreTypeProperty());
 	}
 	
 	public final IBlockState getBlockState(OreType type)
 	{
-		return this.getBlockState().getProperties().isEmpty() ? this.getDefaultState() : this.getDefaultState().withProperty(this.property, type);
+		return this.getBlockState().getProperties().isEmpty() ? this.getDefaultState() : this.getDefaultState().withProperty(this.getOreTypeProperty(), type);
 	}
 	
 	public final OreType getOreType(int index)
 	{
-		return this.property.getAllowedValues().get(index);
+		return this.getOreTypeProperty().getTypes().getOreTypes().get(index);
 	}
 	
 	public final int getIndex(OreType type)
 	{
-		return type.getTypes() == this.getOreTypeProperty().getTypes() ? this.getBlockState().getProperties().isEmpty() ? 0 : this.property.getAllowedValues().indexOf(type) : -1;
+		return type.getTypes() == this.getOreTypeProperty().getTypes() ? this.getBlockState().getProperties().isEmpty() ? 0 : this.getOreTypeProperty().getTypes().getOreTypes().indexOf(type) : -1;
+	}
+	
+	public static Function<Pair<OreMaterial, OreTypes>, String> getDefaultRegistryNameGetter()
+	{
+		return pair -> pair.getLeft().getRegistryName().toString() + "." + pair.getRight().getRegistryName().toString().replaceFirst(":", "_");
+	}
+	
+	public static class SimpleImpl extends BlockOre
+	{
+		private final OreMaterial.Impl impl;
+		
+		public SimpleImpl(OreMaterial.Impl material, OreTypes types)
+		{
+			super(material, types);
+			this.impl = material;
+		}
+		
+		public SimpleImpl(OreMaterial.Impl material, OreTypes types, Function<Pair<OreMaterial, OreTypes>, String> registry_name_getter)
+		{
+			super(material, types, registry_name_getter);
+			this.impl = material;
+		}
+		
+		@Override
+		public Item getItemDropped(IBlockState state, Random random, int fortune)
+		{
+			return this.impl.getDrop();
+		}
+		
+		@Override
+		public int quantityDropped(Random random)
+		{
+			return this.impl.getDropCount(random);
+		}
+		
+		@Override
+		public int quantityDroppedWithBonus(int fortune, Random random)
+		{
+			return this.impl.getDropCountWithFortune(fortune, random);
+		}
+		
+		@Override
+		public int damageDropped(IBlockState state)
+		{
+			int meta = this.impl.getDropMeta();
+			return meta == -1 ? OreUtils.getItemStackForMaterial(this.impl).getMetadata() : meta;
+		}
+		
+		@Override
+		public int getExpDrop(IBlockState state, IBlockAccess access, BlockPos pos, int fortune)
+		{
+			return this.impl.getDropXP(access instanceof World ? ((World)access).rand : Block.RANDOM);
+		}
 	}
 }
---
+++ src/main/java/api/metalextras/SPacketBlockOreLandingParticles.java
@@ -0,0 +1,91 @@
+package api.metalextras;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+
+public class SPacketBlockOreLandingParticles implements IMessage
+{
+	private OreMaterial material;
+	private OreType type;
+	private Vec3d pos;
+	private int particles;
+	
+	public SPacketBlockOreLandingParticles()
+	{
+		
+	}
+	
+	public SPacketBlockOreLandingParticles(OreMaterial material, OreType type, Vec3d pos, int particles)
+	{
+		this.material = material;
+		this.type = type;
+		this.pos = pos;
+		this.particles = particles;
+	}
+	
+	@Override
+	public void fromBytes(ByteBuf buffer)
+	{
+		this.material = OreUtils.getMaterialsRegistry().getValue(new ResourceLocation(ByteBufUtils.readUTF8String(buffer)));
+		this.type = OreUtils.findOreType(new ResourceLocation(ByteBufUtils.readUTF8String(buffer)));
+		this.pos = new Vec3d(buffer.readDouble(), buffer.readDouble(), buffer.readDouble());
+		this.particles = buffer.readInt();
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		ByteBufUtils.writeUTF8String(buffer, this.material.getRegistryName().toString());
+		ByteBufUtils.writeUTF8String(buffer, this.type.getRegistryName().toString());
+		buffer.writeDouble(this.pos.xCoord);
+		buffer.writeDouble(this.pos.yCoord);
+		buffer.writeDouble(this.pos.zCoord);
+		buffer.writeInt(this.particles);
+	}
+	
+	public OreMaterial getOreMaterial()
+	{
+		return this.material;
+	}
+	
+	public OreType getOreType()
+	{
+		return this.type;
+	}
+	
+	public Vec3d getPosition()
+	{
+		return this.pos;
+	}
+	
+	public int getParticles()
+	{
+		return this.particles;
+	}
+	
+	public static class SendLandingParticlesEvent extends Event
+	{
+		private final int dimension;
+		private final SPacketBlockOreLandingParticles message;
+		
+		public SendLandingParticlesEvent(int dimension, SPacketBlockOreLandingParticles message)
+		{
+			this.dimension = dimension;
+			this.message = message;
+		}
+		
+		public int getDimension()
+		{
+			return this.dimension;
+		}
+		
+		public SPacketBlockOreLandingParticles getMessage()
+		{
+			return this.message;
+		}
+	}
+}
